436762316edad55dc779a297200bd073881f5030
sykung6 <78710218+sykung6@users.noreply.github.com>
2021-02-08 09:16:09 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,17 @@
+function ShowMyName () {
+    basic.showString("My Name is:\"")
+    basic.pause(300)
+    basic.showString("Super Coder")
+}
+ShowMyName()
+basic.showLeds(`
+    . . . . .
+    . # . # .
+    . . . . .
+    # . . . #
+    . # # # .
+    `)
+ShowMyName()
+basic.forever(function () {
+	
+})
